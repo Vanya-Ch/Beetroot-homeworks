93237b2f5a29a339f6b731ff3b3a5812db74a71e
Vanya-Ch <mr.vanya882@gmail.com>
2022-10-29 22:15:33 +0300

    JS-1

---
+++ JS-1/assets/js/script.js
@@ -0,0 +1,21 @@
+let userName = prompt('What is your name?');
+console.log('Привіт ' + userName);
+let userBorn = prompt('What year you were born?');
+const YEAR = 2022;
+let userAge = YEAR - userBorn;
+console.log(userAge);
+let square = prompt('Set the side of the square');
+console.log(square**2);
+let radius = prompt('Set circle radius');
+let circleSquare = radius**2 * 3.14;
+console.log(circleSquare);
+
+let distance = prompt('How far are you planning to travel (kilometers)?')
+let time = prompt('How many time are you planning to spend (hours)?')
+let speed = distance/time;
+console.log('You should move '+ speed + ' km/h')
+
+let dollar = prompt('How many dollars you want to change?');
+const EXCHANGE_RATE = 1.3;
+let euro = dollar * EXCHANGE_RATE;
+console.log("Here your's " + euro + " euro");
