8c42382c4ef5ab0d285eccee775e909e04b30e31
Vanya-Ch <mr.vanya882@gmail.com>
2022-11-23 13:47:23 +0200

    Slider swiper

--- JS-9/assets/js/script.js
+++ JS-9/assets/js/script.js
@@ -1,9 +1,11 @@
 
    var swiper = new Swiper(".my-swiper", {
+    loop: true,
     pagination: {
       el: ".swiper-pagination",
       clickable : true,
     },
+   // background: 
   });
   
   
