cd1d68c2aeda76cb44e967eb84c525e5e654364c
Vanya-Ch <mr.vanya882@gmail.com>
2022-11-25 20:38:26 +0200

    Monticello

--- Monticello/assets/js/script.js
+++ Monticello/assets/js/script.js
@@ -1,22 +1,38 @@
+let headerPagination = document.querySelector('.header-pagination')
+
 let swiperHeader = new Swiper(".header__swiper", {
     direction: "vertical",
     pagination: {
-      el: ".swiper-pagination",
+      el: headerPagination,
       clickable: true,
     },
     loop:true,
   });
+headerPagination.style.right = "52px";
+headerPagination.style.transform ="none";
+headerPagination.style.top ="unset";
+
+
+let newsPagination = document.querySelector('.swiper-news-pagination'),
+    newsNext = document.querySelector('.swiper-news-next'),
+    newsPrev = document.querySelector('.swiper-news-prev')
 
 let swiperNews = new Swiper(".swiper-news", {
     slidesPerView: 3,
     spaceBetween: 30,
     loop:true,
     pagination: {
-      el: ".swiper-pagination",
+      el: newsPagination,
       clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: newsNext,
+      prevEl: newsPrev,
     },
 });
+
+newsPagination.style.bottom = "120px";
+newsNext.style.right = "115px";
+newsPrev.style.left = "115px"
+newsPrev.style.top = "unset";
+newsNext.style.top = "unset";
